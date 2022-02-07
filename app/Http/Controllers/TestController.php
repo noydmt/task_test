c55d64fa739c3bb5f0eded6d169c42dace57e170
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-02-07 23:43:21 +0900

    モデルクラスを参照してテーブルの中身を全て引っ張る

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Test; // モデルクラスを参照
+
 class TestController extends Controller
 {
     public function index() {
-      return view('tests.test');
+      $values = Test::all();
+      // dd($values); // 処理を停止して変数の中身をデバックできる
+      return view('tests.test', compact('values'));
     }
 }
