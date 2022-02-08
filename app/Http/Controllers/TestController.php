c3d7565343f3bfd4338668342704d49f78aa00e7
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-02-08 22:04:51 +0900

    クエリビルダを使用してDBからデータを表示

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Test; // モデルクラスを参照
+use Illuminate\Support\Facades\DB; // クエリビルダ使用するためにDBファサードを参照
 
 class TestController extends Controller
 {
     public function index() {
+      $tests = DB::Table('tests')->get();
       $values = Test::all();
+      dd($tests);
       // dd($values); // 処理を停止して変数の中身をデバックできる
       return view('tests.test', compact('values'));
     }
