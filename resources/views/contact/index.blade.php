f7a269086e2c43245d6069d8bf6593c34d40dda2
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-02-14 22:11:29 +0900

    homeページを参考に新ページを追加

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,4 +1,23 @@
-<h2>index page of contact</h2>
-<?php
+@extends('layouts.app')
 
-?>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    This is index of content;
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
