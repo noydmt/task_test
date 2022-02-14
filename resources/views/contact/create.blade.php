10100a9a072b1801af1a79512fce01ed4cac784c
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-02-14 22:53:14 +0900

    create ページにサンプルフォーム作成

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -2,22 +2,111 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    This is create of content;
-                </div>
+        <div class="card-body">
+          @if (session('status'))
+            <div class="alert alert-success" role="alert">
+              {{ session('status') }}
             </div>
+          @endif
+          <div class="row">
+            <form method="POST" action="input.php">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="your_name">氏名</label>
+                  <input type="text" class="form-control" id="your_name" name="your_name" value="<?php if(!empty($_POST['your_name'] )) { echo h($_POST['your_name']); }?>" required>
+                </div>
+                <div class="form-group">
+                  <label for="email">e-mail</label>
+                  <input type="email" class="form-control" id="email" name="email" value="<?php if (!empty($_POST['email'])) { echo h($_POST['email']); }?>" required>
+                </div>
+                <div class="form-group">
+                  <label for="url">ホームページ</label>
+                  <input type="url" class="form-control" id="url" name="url" value="<?php if (!empty($_POST['url'])) { echo h($_POST['url']); }?>" required>
+                </div>
+                <label>性別</label><br>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" id="man" name="gender" value="0" <?php if (isset($_POST['gender']) && h($_POST['gender']) === '0') { echo 'checked'; } ?>>
+                <label class="form-check-label" for="man">男</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" id="woman" name="gender" value="1" <?php if (isset($_POST['gender']) && h($_POST['gender']) === '1') { echo 'checked'; } ?>>
+                  <label class="form-check-label" for="woman">女</label>
+                </div>
+                <div class="form-group">
+                  <label for="age">年齢</label>
+                  <select class="form-control" name="age">
+                    <option value="0"
+                      <?php
+                          if (isset($_POST['age']) && $_POST['age'] === '0') {
+                          echo 'selected';
+                          }
+                      ?>
+                    >選択してください。
+                    </option>
+                    <option value="1"
+                      <?php
+                          if (isset($_POST['age']) && $_POST['age'] === '1') {
+                          echo 'selected';
+                          }
+                      ?>
+                    >15~19
+                    </option>
+                    <option value="2"
+                      <?php
+                          if (isset($_POST['age']) && $_POST['age'] === '2') {
+                          echo 'selected';
+                          }
+                      ?>
+                    >20~24
+                    </option>
+                    <option value="3"
+                      <?php
+                          if (isset($_POST['age']) && $_POST['age'] === '3') {
+                          echo 'selected';
+                          }
+                      ?>
+                      >25~29
+                    </option>
+                    <option value="4"
+                      <?php
+                          if (isset($_POST['age']) && $_POST['age'] === '4') {
+                          echo 'selected';
+                          }
+                      ?>
+                    >30~34
+                    </option>
+                    <option value="5"
+                      <?php
+                          if (isset($_POST['age']) && $_POST['age'] === '5') {
+                          echo 'selected';
+                          }
+                      ?>
+                    >35~
+                    </option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="contact">お問い合わせ</label>
+                  <textarea class="form-control" id="contact" name="contact"><?php if (!empty($_POST['contact'])) { echo h($_POST['contact']); }?></textarea>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="caution" name="caution" value="1">
+                  <label class="form-check-label" for="caution">注意事項</label>
+                </div>
+              </div>
+              <input class="btn btn-primary" type="submit" name="btn_confirm" value="確認する">
+              <input type="hidden" name="csrf">
+            </form>
+          </div>
+              This is create of content;
         </div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
