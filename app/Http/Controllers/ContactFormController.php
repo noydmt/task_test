c0fe760bed440bcabc567194181a470c330fd4ca
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-02-23 01:09:38 +0900

    delete処理を実装

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -156,6 +156,8 @@ class ContactFormController extends Controller
      */
     public function destroy($id)
     {
-        //
+      $contact = ContactForm::find($id);
+      $contact->delete();
+      return redirect('contact/index');
     }
 }
