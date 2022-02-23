977f8a2ced434e53d822f62619b24c0787b4ffb0
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-02-23 22:08:47 +0900

    バリデーションエラーメッセージを表示

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -13,6 +13,16 @@
               {{ session('status') }}
             </div>
           @endif
+
+          @if ($errors->any())
+          <div class="alert alert-danger">
+            <ul>
+              @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+              @endforeach
+            </ul>
+          </div>
+          @endif
           <div class="row">
             <form method="POST" action="{{ route('contact.store') }}">
               @csrf
