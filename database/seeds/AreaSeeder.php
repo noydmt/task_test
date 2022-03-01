36e8f331b77e1d3384b40c2fb961f669448b045e
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-03-01 22:16:28 +0900

    Areasテーブルのダミーデータを準備

--- database/seeds/AreaSeeder.php
+++ database/seeds/AreaSeeder.php
@@ -4,13 +4,36 @@ use Illuminate\Database\Seeder;
 
 class AreaSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$params = [
+			[
+				'id' => 1,
+				'name' => '東京都',
+				'sort_no' => 1,
+			],
+			[
+				'id' => 2,
+				'name' => '大阪府',
+				'sort_no' => 2,
+			],
+			[
+				'id' => 3,
+				'name' => '福岡県',
+				'sort_no' => 3,
+			]
+		];
+
+		$now = Carbon::now();
+		foreach ($params as $param) {
+			$param['created_at'] = $now;
+			$param['updated_at'] = $now;
+			DB::table('areas')->insert($param);
+		};
+	}
 }
