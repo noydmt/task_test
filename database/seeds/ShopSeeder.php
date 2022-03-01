bbda51506207acfa290a74c49eeedd008b5a80e5
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-03-01 22:23:25 +0900

    shopsテーブルのダミーデータを準備

--- database/seeds/ShopSeeder.php
+++ database/seeds/ShopSeeder.php
@@ -11,6 +11,39 @@ class ShopSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $params = [
+            [
+                'id' => 1,
+                'shop_name' => '高級食パン店',
+                'area_id' => 1,
+            ],
+            [
+                'id' => 1,
+                'shop_name' => '高級食パン店',
+                'area_id' => 1,
+            ],
+            [
+                'id' => 2,
+                'shop_name' => '高級食パン店',
+                'area_id' => 1,
+            ],
+            [
+                'id' => 3,
+                'shop_name' => '高級食パン店',
+                'area_id' => 1,
+            ],
+            [
+                'id' => 2,
+                'shop_name' => '高級食パン店',
+                'area_id' => 1,
+            ],
+        ];
+
+        $now = Carbon::now();
+        foreach ($params as $param) {
+            $param['created_at'] = $now;
+            $param['updated_at'] = $now;
+            DB::table('shops')->insert($param);
+        }
     }
 }
