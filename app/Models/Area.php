2719f528bb60f8da93986356876fd9ed4f8bcc82
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-03-01 23:07:34 +0900

    モデルのリレーション関係を記述

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Shop;
 
 class Area extends Model
 {
-    //
+    public function shops() {
+        return $this->hasMany(Shop::class);
+    }
 }
--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Area;
 
 class Shop extends Model
 {
-    //
+    public function Area() {
+        return $this->belongsTo(Area::class);
+    }
 }
